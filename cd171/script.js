5c5ba34e378e5173f76c07e2c4408043c6bcc03d
Daniil Iljin <daniil@metaprint.com>
2024-08-14 13:17:09 +0300

    add smth

--- cd171/script.js
+++ cd171/script.js
@@ -1,14 +1,52 @@
- // p5.js setup function
- function setup() {
-    // Create a canvas of size 400x400
-    createCanvas(600, 600);
-  }
 
-  // p5.js draw function
-  function draw() {
-    // Set background color to light gray
-    background(220);
 
-    // Draw a circle at the mouse position
-    ellipse(mouseX, mouseY, 50, 50);
-  }
+const tiles = [];
+
+const grid = [];
+const DIM = 2;
+
+const BLANK = 0
+const UP = 1
+const RIGHT = 2
+const DOWN = 3
+const LEFT = 4
+
+function preload() {
+    tiles[0] = loadImage("demo/blank.png");
+    tiles[1] = loadImage("demo/up.png");
+    tiles[2] = loadImage("demo/right.png");
+    tiles[3] = loadImage("demo/down.png");
+    tiles[4] = loadImage("demo/left.png");
+}
+
+function setup() {
+    createCanvas(400, 400);
+    for (let i = 0; i < DIM * DIM; i++) {
+      grid[i] = {
+        collapsed: false,
+        options: [BLANK, UP, RIGHT, DOWN, LEFT] 
+      }
+    }
+
+    grid[0].collapsed = true
+    grid[0].options = [UP]
+}
+
+function draw() {
+    background(0);
+    const w = width / DIM
+    const h = height / DIM
+    for (let j = 0; j < DIM; j++) {
+      for (let i = 0; i < DIM; i++) {
+        let cell = grid[i + j * DIM]
+        if (cell.collapsed) {
+          let index = cell.options[0]
+          image(tiles[index], i * w, j * h, w, h)
+        } else {
+          fill(0)
+          stroke(255)
+          rect(i * w, j * h, w, h)
+        }
+      }
+    }
+}
